8a42bc8b9eb5a506e3a5f195a64615da76834ec2
hooponn <neo-hp@163.com>
2016-06-21 05:10:30 +0800

    change searchactivity
    加入代码

--- 游记/src/com/ghllz/travel/ui/SearchActivity.java
+++ 游记/src/com/ghllz/travel/ui/SearchActivity.java
@@ -1,16 +1,25 @@
 package com.ghllz.travel.ui;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.ghllz.travel.R;
+import com.ghllz.travel.adapter.SearchAdapter;
 import com.ghllz.travel.bean.Place;
 import com.ghllz.travel.presenter.IPlaceListPresenter;
 import com.ghllz.travel.presenter.PlaceListPresenterImpl;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class SearchActivity extends BaseActivity implements IPlaceListView{
-
+public class SearchActivity extends BaseActivity implements IPlaceListView, TextWatcher{
+	EditText et_search;
+	ListView listview;
+	SearchAdapter sa;
+	List<Place> places,list;
 	IPlaceListPresenter mPresenter;
 	
 	@Override
@@ -22,6 +31,13 @@ public class SearchActivity extends BaseActivity implements IPlaceListView{
 	
 	private void init() {
 		mPresenter = new PlaceListPresenterImpl(this);
+		et_search=(EditText) findViewById(R.id.search_et);
+		listview=(ListView) findViewById(R.id.search_listView);
+		places=new ArrayList<Place>();
+		list=new ArrayList<Place>();
+		sa=new SearchAdapter(this,places);
+		listview.setAdapter(sa);
+		et_search.addTextChangedListener(this);
 	}
 
 	@Override
@@ -31,7 +47,38 @@ public class SearchActivity extends BaseActivity implements IPlaceListView{
 	}
 	@Override
 	public void showPlaceList(List<Place> places) {
-		
+		this.places=places;
+	}
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+	}
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+	}
+	@Override
+	public void afterTextChanged(Editable s) {
+		if(s.toString().length()>0){
+			list.clear();
+			for(Place p:places){
+				/*int i;
+				for(i=0;i<=p.getCity().length();i++){
+					if(s.toString().equals(p.getCity().subSequence(0,i))||
+							s.toString().equals(p.getLowerCase().substring(0,i))||
+							s.toString().equals(p.getUpperCase().substring(0,i)))
+					{
+						list.add(p);
+					}
+				}*/
+				if(p.getCity().contains(s.toString())||
+						p.getLowerCase().contains(s.toString())||
+						p.getUpperCase().contains(s.toString())){
+					list.add(p);
+				}
+			}
+		}else{
+		list.clear();
+		}
+		sa.refresh(list,s.toString());
 	}
 
 }
